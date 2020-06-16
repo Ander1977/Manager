6bce2dc40363cbf09169416dadddc6e2a8164fd1
Ander1977 <kopa.andrej@gmail.com>
2020-06-16 14:42:49 +0300

    Added branch ProductManager

---
+++ src/main/java/ru/netology/Manager/ProductManager.java
@@ -0,0 +1,4 @@
+package ru.netology.Manager;
+
+public class ProductManager {
+}
---
+++ src/main/java/ru/netology/repository/Productrepository.java
@@ -0,0 +1,61 @@
+package ru.netology.repository;
+
+import ru.netology.domaun.Book;
+import ru.netology.domaun.Product;
+import ru.netology.domaun.Smartphone;
+
+public class Productrepository {
+
+    private Book[] books = new Book[0];
+    private Smartphone[] smartphones = new Smartphone[0];
+    Product[] products = new Product[0];
+
+    public Product[] findAll() {
+        Product[] products = new Product[books.length + smartphones.length];
+        System.arraycopy(books, 0, products, 0, books.length);
+        System.arraycopy(smartphones, 0, products, books.length, smartphones.length);
+        return products;
+    }
+
+    public void save(Book book) {
+        int length = books.length + 1;
+        Book[] tmp = new Book[length];
+        System.arraycopy(books, 0, tmp, 0, books.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = book;
+        tmp = books;
+    }
+
+    public void save(Smartphone smartphone) {
+        int length = smartphones.length + 1;
+        Smartphone[] tmp = new Smartphone[length];
+        System.arraycopy(smartphones, 0, tmp, 0, smartphones.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = smartphone;
+        tmp = smartphones;
+    }
+
+    public void removeById(int id) {
+        int lengthBook = books.length - 1;
+        Book[] tmpBook = new Book[lengthBook];
+        for (Book book : books) {
+            if (book.getId() != id) {
+                int index = 0;
+                tmpBook[index] = book;
+                index++;
+            }
+        }
+        tmpBook = books;
+
+        int lengthSmart = smartphones.length;
+        Smartphone[] tmpSmart = new Smartphone[lengthSmart];
+        for (Smartphone smartphone : smartphones) {
+            if (smartphone.getId() != id) {
+                int index = 0;
+                tmpSmart[index] = smartphone;
+                index++;
+            }
+        }
+        tmpSmart = smartphones;
+    }
+}
