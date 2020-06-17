fefb1b6813a221d83aad3bd85b979f9cbae06163
Ander1977 <kopa.andrej@gmail.com>
2020-06-17 15:54:17 +0300

    Change Repository

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -1,61 +1,36 @@
 package ru.netology.repository;
 
-import ru.netology.domain.Book;
+
 import ru.netology.domain.Product;
-import ru.netology.domain.Smartphone;
+
 
 public class ProductRepository {
-
-    private Book[] books = new Book[0];
-    private Smartphone[] smartphones = new Smartphone[0];
     Product[] products = new Product[0];
 
+
     public Product[] findAll() {
-        Product[] products = new Product[books.length + smartphones.length];
-        System.arraycopy(books, 0, products, 0, books.length);
-        System.arraycopy(smartphones, 0, products, books.length, smartphones.length);
         return products;
     }
 
-    public void save(Book book) {
-        int length = books.length + 1;
-        Book[] tmp = new Book[length];
-        System.arraycopy(books, 0, tmp, 0, books.length);
+    public void save(Product product) {
+        int length = products.length + 1;
+        Product[] tmp = new Product[length];
+        System.arraycopy(products, 0, tmp, 0, products.length);
         int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = book;
-        tmp = books;
-    }
-
-    public void save(Smartphone smartphone) {
-        int length = smartphones.length + 1;
-        Smartphone[] tmp = new Smartphone[length];
-        System.arraycopy(smartphones, 0, tmp, 0, smartphones.length);
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = smartphone;
-        tmp = smartphones;
+        tmp[lastIndex] = product;
+        tmp = products;
     }
 
     public void removeById(int id) {
-        int lengthBook = books.length - 1;
-        Book[] tmpBook = new Book[lengthBook];
-        for (Book book : books) {
-            if (book.getId() != id) {
+        int lengthProduct = products.length - 1;
+        Product[] tmpProduct = new Product[lengthProduct];
+        for (Product product : products) {
+            if (product.getId() != id) {
                 int index = 0;
-                tmpBook[index] = book;
+                tmpProduct[index] = product;
                 index++;
             }
         }
-        tmpBook = books;
-
-        int lengthSmart = smartphones.length;
-        Smartphone[] tmpSmart = new Smartphone[lengthSmart];
-        for (Smartphone smartphone : smartphones) {
-            if (smartphone.getId() != id) {
-                int index = 0;
-                tmpSmart[index] = smartphone;
-                index++;
-            }
-        }
-        tmpSmart = smartphones;
+        tmpProduct = products;
     }
 }
